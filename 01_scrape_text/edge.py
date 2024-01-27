087f9a5d267b302519454c8eef85db40c49f90eb
simzeee <jwsims816@gmail.com>
2024-01-27 01:07:13 -0500

    first commit: complete scrape static text and initialize repo

---
+++ 01_scrape_text/edge.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+ 
+def get_drvier():
+  # Set options to make browsing easier
+  print('here')
+  options = webdriver.EdgeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+ 
+  driver = webdriver.Edge(options=options)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
+ 
+def main():
+  driver = get_drvier()
+  element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
+  return element.text
+ 
+print(main())
